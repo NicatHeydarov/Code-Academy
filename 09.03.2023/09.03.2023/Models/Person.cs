0f36e513b0c64214ef2065626075bcf257ee9203
NIcat <nijatshh@code.edu.az>
2023-03-10 16:52:16 +0400

    homework

---
+++ 09.03.2023/09.03.2023/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿
+namespace _09._03._2023.Models
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public byte Age { get; set; }
+
+        public Person(string name,string Surname,byte Age)
+        {
+            this.Name = name;
+            this.Surname = Surname;
+            this.Age = Age;
+        }
+
+        public string ShowInfo(string name,int age) 
+        {
+        return $"{name},{Surname} ,{Age}";
+        }
+
+    }
+}
---
+++ 09.03.2023/09.03.2023/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿
+namespace _09._03._2023.Models
+{
+    internal class Student : Person
+    {
+        public byte _point
+        {
+            get
+            {
+                return _point;
+            }
+            set
+            {
+                if (value > 0 && value <= 100)
+                {
+                    _point = value;
+                }
+                else
+                {
+                    _point = 0;
+                }
+            }
+        }
+        public Student(string name, byte age, string surname, byte _point) : base(name, surname, age)
+        {
+
+            this.Name = name;
+            this.Age = age;
+            this.Surname = surname;
+            this._point = _point;
+        }
+    }
+
+}
